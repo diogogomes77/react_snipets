dcc14638a20bed12922c18f1ced6d1967c8d7474
Diogo Gomes <diogo.gomes77@gmail.com>
2022-03-01 19:20:16 +0000

    feat(hooks/context): add Router and some pages

--- packages/reactHooks/src/App.jsx
+++ packages/reactHooks/src/App.jsx
@@ -1,11 +1,31 @@
 import Timer from './components/Timer';
 import React from 'react';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Index from 'pages';
+import About from 'pages/about';
 
 function App() {
   return (
     <div className='App'>
-      <Timer />
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/timer'>Timer example</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/timer' element={<Timer />} />
+      </Routes>
     </div>
   );
 }
